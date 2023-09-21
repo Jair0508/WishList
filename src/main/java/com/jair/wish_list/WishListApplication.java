6f5c4bc33ceefb5227d60d932dc38cacbdb1d021
Jair0508 <jairmm0508@gmail.com>
2023-09-21 01:35:07 -0500

    My first commit.

---
+++ src/main/java/com/jair/wish_list/WishListApplication.java
@@ -0,0 +1,13 @@
+package com.jair.wish_list;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WishListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WishListApplication.class, args);
+	}
+
+}
